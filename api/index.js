501e8dc698c94ab0ee7f4f03d3e28e54c9664c74
btcxinchao <buitanchuongg@gmail.com>
2026-09-18 16:00:25 +0700

    khởi tạo projec và nhúng file tĩnh

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+
+//thiết lập pug làm view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// thiết lập thư mục public để chứa các file tĩnh (CSS, JS, hình ảnh)
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Trang chủ !')
-})
+  res.render('client/pages/home');
+});
 
-
+app.get('/tour', (req, res) => {
+  console.log('GET /tour đã chạy');
+  res.render('client/pages/tourlist',{pageTitle: 'Tour List'});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server đang chạy: http://localhost:${port}`);
+});
